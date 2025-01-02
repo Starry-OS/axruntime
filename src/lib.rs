6929156445bad55fd0cfaee707a66a2318545132
baiqiaosen <baiqiaosen@gmail.com>
2025-01-02 06:51:45 +0000

    disable network temporarily

--- src/lib.rs
+++ src/lib.rs
@@ -161,7 +161,7 @@ pub extern "C" fn rust_main(cpu_id: usize, dtb: usize) {
         axfs::init_filesystems(all_devices.block);
 
         #[cfg(feature = "net")]
-        axnet::init_network(all_devices.net);
+        //axnet::init_network(all_devices.net);
 
         #[cfg(feature = "display")]
         axdisplay::init_display(all_devices.display);
